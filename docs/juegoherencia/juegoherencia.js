c5a6f858d8bbaa95e5e6b7db67c7f80329b31883
tob <tobiasalbirosa@gmail.com>
2023-11-02 19:21:35 -0300

    Probando p5js en GitHub pages

---
+++ docs/juegoherencia/juegoherencia.js
@@ -0,0 +1,18 @@
+let juego
+
+function setup() {
+  //createCanvas(windowWidth, windowHeight)
+  juego = new Juego()
+  
+}
+
+
+function draw() {
+  juego.dibujar()
+}
+
+
+function keyPressed(){
+
+    juego.keyPressed()
+}
